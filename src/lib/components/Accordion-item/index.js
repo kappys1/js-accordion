4b400b49270a9efb92606678961f11b5ff23fad0
alex marcos <alexsbd1@gmail.com>
2019-08-25 12:56:38 +0200

    feat(themes): add themes on example page

--- src/lib/components/Accordion-item/index.js
+++ src/lib/components/Accordion-item/index.js
@@ -23,6 +23,7 @@ export class AccordionItem {
   close() {
     this.opened = false
     this.header.classList.remove('JsAccordion-header--opened')
+    this.content.classList.remove('JsAccordion-body--opened')
     if (this.content) {
       this.content.style.height = '0px'
     }
@@ -31,6 +32,7 @@ export class AccordionItem {
   open() {
     this.opened = true
     this.header.classList.add('JsAccordion-header--opened')
+    this.content.classList.add('JsAccordion-body--opened')
     if (this.content) {
       this.content.style.height = `${this.content.scrollHeight}px`
     }
